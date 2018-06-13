2fcc957454164fdb56d94ceaba72eff7ed9ef0e8
ssingularity <1755405704@qq.com>
2018-06-13 12:27:39 +0800

    add the gateway

--- microservice-consumer/src/main/java/com/microservice/consumer/service/ConsumerService.java
+++ microservice-consumer/src/main/java/com/microservice/consumer/service/ConsumerService.java
@@ -2,6 +2,7 @@ package com.microservice.consumer.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +12,17 @@ public class ConsumerService {
 	@Autowired
 	RestTemplate restTemplate;
 
+	private final Logger logger = Logger.getLogger(getClass());
+
 	@HystrixCommand(fallbackMethod = "consumerFallback")
 	public String consumeProvider(){
-		return restTemplate.getForObject("http://hello-service/pipipan",String.class);
+		long start = System.currentTimeMillis();
+
+		String result = restTemplate.getForObject("http://hello-service/pipipan",String.class);
+
+		long end = System.currentTimeMillis();
+		logger.info("Spend time : " + (end - start));
+		return result;
 	}
 
 	public String consumerFallback(){
